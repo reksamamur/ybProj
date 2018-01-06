12806d5c7172a49fb743a618c912311549bad57a
Muhammad Alief <alief.pribadi@gmail.com>
2018-01-06 18:01:33 +0700

    REGISTER SELESAI (Volley) Tinggal bagian kode otomatis sebagai pengenal pengawas

--- app/src/main/java/digyb/la03/ybprog/Config.java
+++ app/src/main/java/digyb/la03/ybprog/Config.java
@@ -8,6 +8,7 @@ public class Config {
 
     //URL REQUEST
     public static final String URL_LOGIN = "http://192.168.100.2/yb/login.php";
+    public static final String URL_REG = "http://192.168.100.2/yb/register.php";
 
     //POST REQUEST
 
@@ -15,7 +16,12 @@ public class Config {
     public static final String LOGIN_USER = "user";
     public static final String LOGIN_PASS = "pass";
 
+    public static final String REG_USER = "rUser";
+    public static final String REG_PASS = "rPass";
+    public static final String REG_EMAIL = "rEmail";
+
     //STATUS
     public static final String LOGIN_ISLOG = "success";
+    public static final String REG_ISREG = "success";
 
 }
--- app/src/main/java/digyb/la03/ybprog/SignupActivity.java
+++ app/src/main/java/digyb/la03/ybprog/SignupActivity.java
@@ -4,20 +4,121 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private EditText edtUser;
+    private EditText edtEmail;
+    private EditText edtPass;
+    private Button btnReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        btnReg = findViewById(R.id.btn_reg);
+
+        edtUser =findViewById(R.id.reg_user);
+
+        edtPass =findViewById(R.id.reg_pass);
+
+        edtEmail =findViewById(R.id.reg_email);
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
     }
 
-    public void btn_SignUp_sgn(View view){
+    public void register() {
+        final String sUser = edtUser.getText().toString().trim();
+        final String sPass = edtPass.getText().toString().trim();
+        final String sEmail = edtEmail.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Config.URL_REG,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        //If we are getting success from server
+                        if (response.contains(Config.REG_ISREG)) {
+//                            hideDialog();
+                            Toast.makeText(SignupActivity.this, "Register Success", Toast.LENGTH_SHORT).show();
+                            gotoMainActivity();
+
+                        } else {
+                            hideDialog();
+                            //Displaying an error message on toast
+//                            Toast.makeText(getApplicationContext(), "Invalid username or password", Toast.LENGTH_LONG).show();
+                            Toast.makeText(SignupActivity.this, response, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //You can handle error here if you want
+                        hideDialog();
+                        Toast.makeText(getApplicationContext(), "The server unreachable", Toast.LENGTH_LONG).show();
+
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                //Adding parameters to request
+                params.put(Config.REG_USER, sUser);
+                params.put(Config.REG_PASS, sPass);
+                params.put(Config.REG_EMAIL, sEmail);
+
+                //returning parameter
+                return params;
+            }
+
+//        Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        finish();
+//        startActivity(intent);
+
+        };
+
+        Volley.newRequestQueue(this).add(stringRequest);
+    }
+
+
+
+    private void gotoMainActivity()
+    {
         Intent intent = new Intent(SignupActivity.this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         finish();
         startActivity(intent);
+    }
+
+    private void showDialog() {
+//        if (!pDialog.isShowing())
+//            pDialog.show();
+    }
+
+    private void hideDialog() {
+//        if (pDialog.isShowing())
+//            pDialog.dismiss();
     }
 
     public void btn_Login_sgn(View view){
