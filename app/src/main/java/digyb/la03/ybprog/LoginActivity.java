a670596f2f1e32aecf02b0913f503347bb66d001
Muhammad Alief <alief.pribadi@gmail.com>
2018-01-06 13:39:39 +0700

    YB kaga ada kabar

--- app/src/main/java/digyb/la03/ybprog/LoginActivity.java
+++ app/src/main/java/digyb/la03/ybprog/LoginActivity.java
@@ -13,11 +13,21 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
+    //mining lelet
+
+    public void gaJelas()
+    {
+
+    }
+
     public void btn_Login(View view){
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         finish();
         startActivity(intent);
+
+        gaJelas();
+
     }
 
     public void btn_SignUp(View view){
