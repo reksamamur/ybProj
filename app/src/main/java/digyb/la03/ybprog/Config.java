89184a3dd4c7dcdde06eae2ea614726f333b2cc7
Muhammad Alief <alief.pribadi@gmail.com>
2018-01-06 16:50:05 +0700

    LOGIN SELESAI (Volley) Server Side Login and config only (localhost)

---
+++ app/src/main/java/digyb/la03/ybprog/Config.java
@@ -0,0 +1,21 @@
+package digyb.la03.ybprog;
+
+/**
+ * Created by Quantum Higgs on 1/6/2018.
+ */
+
+public class Config {
+
+    //URL REQUEST
+    public static final String URL_LOGIN = "http://192.168.100.2/yb/login.php";
+
+    //POST REQUEST
+
+    // YG DI DALAM STRING SESUAIKAN DENGAN $_POST DI SIDE SERVER
+    public static final String LOGIN_USER = "user";
+    public static final String LOGIN_PASS = "pass";
+
+    //STATUS
+    public static final String LOGIN_ISLOG = "success";
+
+}
--- app/src/main/java/digyb/la03/ybprog/LoginActivity.java
+++ app/src/main/java/digyb/la03/ybprog/LoginActivity.java
@@ -1,34 +1,126 @@
 package digyb.la03.ybprog;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText pass;
+    private Button btn_login;
+    private ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        pDialog = new ProgressDialog(getApplicationContext());
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn_Login();
+            }
+        });
+
     }
 
-    //mining lelet
 
-    public void gaJelas()
+    public void btn_Login(){
+
+//        boolean isSuccess = false;
+
+        email = findViewById(R.id.edt_email);
+        pass = findViewById(R.id.edt_pass);
+
+
+        final String sEmail = email.getText().toString().trim();
+        final String sPass = pass.getText().toString().trim();
+
+//        pDialog.setMessage("Tunggu Sebentar...");
+//        showDialog();
+
+        //Creating a string request
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Config.URL_LOGIN,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        //If we are getting success from server
+                        if (response.contains(Config.LOGIN_ISLOG)) {
+                            hideDialog();
+                            gotoMainActivity();
+
+                        } else {
+                            hideDialog();
+                            //Displaying an error message on toast
+                            Toast.makeText(getApplicationContext(), "Invalid username or password", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //You can handle error here if you want
+                        hideDialog();
+                        Toast.makeText(getApplicationContext(), "The server unreachable", Toast.LENGTH_LONG).show();
+
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                //Adding parameters to request
+                params.put(Config.LOGIN_USER, sEmail);
+                params.put(Config.LOGIN_PASS, sPass);
+
+                //returning parameter
+                return params;
+            }
+        };
+
+        //Adding the string request to the queue
+        Volley.newRequestQueue(this).add(stringRequest);
+
+    }
+
+    private void gotoMainActivity()
     {
-
-    }
-
-    public void btn_Login(View view){
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         finish();
         startActivity(intent);
-
-        gaJelas();
-
     }
+
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+
 
     public void btn_SignUp(View view){
         Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
