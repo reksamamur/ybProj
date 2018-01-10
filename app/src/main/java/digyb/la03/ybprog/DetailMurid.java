e28c8c5f188c58ffd6047a87c3d364412cfe14ff
reksamamur <reksa.kuana9@gmail.com>
2018-01-11 00:32:33 +0700

    WAKE UPPPPPP

--- app/src/main/java/digyb/la03/ybprog/DetailMurid.java
+++ app/src/main/java/digyb/la03/ybprog/DetailMurid.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -44,6 +45,13 @@ public class DetailMurid extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
 
 
         a = findViewById(R.id.hasilNol);
